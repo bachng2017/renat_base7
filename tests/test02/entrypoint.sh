5da54cf875d5dd706acb8334727dbe381e49c278
Nguyen Huu Bach <bachng@gmail.com>
2020-11-08 21:00:34 -0500

    update test02

--- tests/test02/entrypoint.sh
+++ tests/test02/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-echo "This is test02"
-echo "---"
-ls -la .
-echo "---"
-ls -la ..
+echo "Test03"
+echo "A null project"
+echo "-------------"
+git clone https://github.com/bachng2017/RENAT.git renat
+export RENAT_PATH renat
+$RENAT_PATH/tools/project.sh sample01
+cd sample01
+$RENAT_PATH/tools/item.sh -b -l test01
+./run.sh
+
