7e217f3336d50f35e79e4db11bdac6bb32c97bf7
Nguyen Huu Bach <bachng@gmail.com>
2020-11-08 21:21:10 -0500

    update test02

--- tests/test02/entrypoint.sh
+++ tests/test02/entrypoint.sh
@@ -5,9 +5,9 @@ echo "A null project"
 echo "-------------"
 git clone https://github.com/bachng2017/RENAT.git renat
 pwd
-echo $WORKSPACE
-ls $WORKSPACE
-export RENAT_PATH renat
+echo $GITHUB_WORKSPACE
+ls $GITHUB_WORKSPACE
+export RENAT_PATH=$GITHUB_WORKSPACE/renat
 $RENAT_PATH/tools/project.sh sample01
 cd sample01
 $RENAT_PATH/tools/item.sh -b -l test01
