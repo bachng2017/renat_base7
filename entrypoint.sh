f18c0bb821df6abae29830c3f9cb44f18dae305d
Nguyen Huu Bach <bachng@gmail.com>
2020-11-10 01:32:36 -0500

    update pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ git clone https://github.com/bachng2017/RENAT.git renat
 echo "Create and run project"
 export RENAT_PATH=$GITHUB_WORKSPACE/renat
 
-for item in $(ls -1 test); do
+TEST_FOLDER=$GITHUB_WORKSPACE/tests
+for item in $(ls -1d $TEST_FOLDER); do
     echo "run test $item"
     cd $item
     ./entrypoint.sh
