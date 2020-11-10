71949db66b2632b08af8601bab7d6ce88e59a6d1
Nguyen Huu Bach <bachng@gmail.com>
2020-11-10 01:45:20 -0500

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Create and run project"
 export RENAT_PATH=$GITHUB_WORKSPACE/renat
 
 TEST_FOLDER=$GITHUB_WORKSPACE/tests
-for item in $(ls -1d $TEST_FOLDER); do
+for item in $(ls -1 $TEST_FOLDER); do
     echo "run test $item"
     cd $item
     ./entrypoint.sh
