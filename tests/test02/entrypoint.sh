e35fde0e022539c38bad8d3419cd42be81146ac4
bachng <bachng@gmail.com>
2020-12-20 01:42:57 +0900

    brush up

--- tests/test02/entrypoint.sh
+++ tests/test02/entrypoint.sh
@@ -5,9 +5,6 @@ echo "Test01"
 echo "This is a null RENAT test case"
 echo "------------------------------"
 
-echo "Clone RENAT source"
-git clone https://github.com/bachng2017/RENAT.git renat
-
 echo "Create and run project"
 export RENAT_PATH=$GITHUB_WORKSPACE/renat
 $RENAT_PATH/tools/project.sh sample01
