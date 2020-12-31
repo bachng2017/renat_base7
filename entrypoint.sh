0e8bf51adf08415596282a57b32806ee80ac287b
bachng <bachng@gmail.com>
2020-12-31 09:33:14 +0900

    using docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
-# This is the entry point for the RENAT base system
-# By default, it will to run every a script call entrypoint.sh in very sub folder under
-# $$TEST_FOLDER
 
-echo "run all test cases in tests folder"
-echo "----------------------------------"
-export RENAT_PATH=$GITHUB_WORKSPACE/renat
+
+echo "+--------------------------------+"
+echo "+ run all shell test cases       +"
+echo "+--------------------------------+"
+
+CODE=0
+
+# Run the test as robot user
+sudo -s -u robot
+export RENAT_PATH=$HOME/work/renat
 
 # default test folder. Need to be an absolute path
-TEST_FOLDER=$GITHUB_WORKSPACE/tests 
-# entrypoint script. Default is entrypoint.sh
-ENTRY_POINT=entrypoint.sh
-RETURN=0
+TEST_FOLDER=$HOME/work/test_shell
+ENTRY_POINT=run.sh
 for item in $(find $TEST_FOLDER -depth  -type f -name $ENTRY_POINT); do
     echo
-    echo
-    echo 
     echo 
     export CURRENT_DIR=$(dirname $item)
     cd $CURRENT_DIR
     echo "Run test in $CURRENT_DIR"
     $item
-    RETURN=$(expr $RETURN + $?)
+    CODE=$(expr $RETURN + $?)
 done
+echo
+echo
+echo
 
-echo "--------------------"
+
+echo "+--------------------------------+"
+echo "+ run all RENAT test cases       +"
+echo "+--------------------------------+"
+cd $HOME/work/test_renat
+./run.sh
+CODE=$(expr $RETURN + $?)
+
+
+echo "---------------------------------"
 echo "Exit code is $RETURN"
-exit $RETURN
-
-
+exit $CODE
 
